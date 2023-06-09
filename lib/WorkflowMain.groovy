70fee574afe66edad0f2b68f63e3721162c66285
mirpedrol <mirp.julia@gmail.com>
2023-06-09 14:49:31 +0200

    more changes

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -1,5 +1,5 @@
 //
-// This file holds several functions specific to the main.nf workflow in the nf-core/testpipeline pipeline
+// This file holds several functions specific to the main.nf workflow in the nf-core/validation pipeline
 //
 
 import nextflow.Nextflow
@@ -48,10 +48,6 @@ class WorkflowMain {
         // Check AWS batch settings
         NfcoreTemplate.awsBatch(workflow, params)
 
-        // Check input has been provided
-        if (!params.input) {
-            Nextflow.error("Please provide an input samplesheet to the pipeline e.g. '--input samplesheet.csv'")
-        }
     }
     //
     // Get attribute from genome config file e.g. fasta
